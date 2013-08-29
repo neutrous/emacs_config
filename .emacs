697170c3678d3ce70b46b196dcbcf4cd8b3f530e
neutrous <neutroustroup@gmail.com>
2013-08-29 18:38:49 +0800

    Initial commit. This commit include basic configuration for my emacs.
    Signed-off-by: neutrous <neutroustroup@gmail.com>

---
+++ .emacs
@@ -0,0 +1,54 @@
+;; tell the emacs to add the path of .emacs.d, then we could
+;; initial/install required packages first.
+(add-to-list 'load-path "~/.emacs.d")
+
+;; Uses el-get to do some magic intialization works!
+(require 'init)
+
+;; Disable auto-back-files
+(setq make-backup-files nil)
+
+;; Disable auto-save-files
+(setq auto-save-default nil)
+
+;; Stop emacs's backup changing the file's creation date of the
+;; original file.
+(setq backup-by-copying t)
+
+;; Allow dired to copy or delete the whole directory.
+;; 'always' means no asking. 'top' means ask once. Any other symbol
+;; means ask each and every time for a dir ans subdir.
+(setq dired-recursive-copies (quote always))
+(setq dired-recursive-deletes (quote top))
+
+;; Copy from one dir to the next dir shown in a split window.
+(setq dired-dwim-target t)
+
+;; Let the 'enter' key and '^' key open the new directory in the same
+; buffer.
+;; (add-hook 'dired-mode-hook
+;;  (lambda ()
+;;   (define-key dired-mode-map (kbd "<return>")
+;;     'dired-find-alternate-file) ; was dired-advertised-find-file
+;;   (define-key dired-mode-map (kbd "^")
+;;     (lambda () (interactive) (find-alternate-file "..")))
+;;   ; was dired-up-directory
+;;  ))
+
+;; ================== Display Setting =================
+;; Display the row number default.
+;; The following setting is conflict with doc-view
+(global-linum-mode t)
+
+;; Display the column number by default.
+(column-number-mode t)
+
+(unless (string-match "x86_64-redhat-linux-gnu" system-configuration)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))))
+
+
---
+++ .emacs.d/init.el
@@ -0,0 +1,59 @@
+;; tell the where the add the el-get package.
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+;; Check whether the el-get package is installed, or else
+;; install it.
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+   (lambda (s)
+     (end-of-buffer)
+     (eval-print-last-sexp))))
+
+;; now either el-get is `require'd already, or have been `load'ed by
+;; the el-get installer.
+(setq el-get-sources		    
+      '(el-get							; el-get is self-hosting
+	escreen							; screen for emacs, C-\ C-h
+	go-mode							; if you're into go...
+	switch-window						; takes over C-x o
+	auto-complete						; complete as you type with overlays
+	zencoding-mode						; http://www.emacswiki.org/emacs/ZenCoding
+	markdown-mode						; the major mode for editing markdown format files.
+	(:name buffer-move					; have to add your own keys.
+	       :after (progn
+			(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+			(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+			(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+			(global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+	
+	(:name evil
+	       :after (progn
+			'(evil-mode t)))
+	
+	;; (:name magit							; git meet emacs, and a binding
+	;;        :after (progn
+	;; 		(global-set-key (kbd "C-x C-z") 'magit-status)))
+	
+	(:name goto-last-change				; move pointer back to last change
+	       :after (progn
+			(global-set-key (kbd "C-x C-/") 'goto-last-change))))
+      )
+
+(unless (string-match "apple-darwin" system-configuration)
+  (loop for p in '(color-theme			; nice looking emacs
+		   color-theme-tango	; check out color-theme-solarized
+		   )
+	do (add-to-list 'el-get-sources p)))
+
+;; Specified the packages which want to synchronized.
+;; I don't why when i added the following, the previous set would install.
+(setq packages
+      (append
+       '(el-get switch-window escreen)
+       (mapcar 'el-get-source-name el-get-sources)))
+
+;; install new packages and init already installed packages
+(el-get 'sync packages)
+
+(provide 'init)
